50b0e2ae49f0c4875496ccd75725d6d9cec6d8bc
iysaofficial <110337759+iysaofficial@users.noreply.github.com>
2022-12-27 11:44:26 +0700

    update

--- components/HomeDemo3/MainBanner.js
+++ components/HomeDemo3/MainBanner.js
@@ -12,9 +12,9 @@ const MainBanner = () => {
               <div className="banner-wrapper-content">
                 <span className="sub-title">IYMIA</span>
                 <h1>International Young Moslem Inventor Award</h1>
-                <a href="https://docs.google.com/forms/d/e/1FAIpQLScXpC2y4PjBWk81Ygmyt4oh3lTVLGjqNPYc-nAMp_GZTSV1WQ/viewform?usp=share_link">
+                {/* <a href="https://docs.google.com/forms/d/e/1FAIpQLScXpC2y4PjBWk81Ygmyt4oh3lTVLGjqNPYc-nAMp_GZTSV1WQ/viewform?usp=share_link">
                   <a className="default-btn">Register Now!</a>
-                </a>
+                </a> */}
               </div>
             </div>
             <div className="col-lg-7 col-md-12">
