31cab49ab3c79070fa0d68fb3f7969c21f008845
iysaofficial <110337759+iysaofficial@users.noreply.github.com>
2023-02-01 11:04:33 +0700

    update faq

--- pages/faq.js
+++ pages/faq.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import NavbarStyleOne from '../components/_App/NavbarStyleOne'
-import NavbarStyleTwo from '@/components/_App/NavbarStyleTwo';
+import NavbarStyleTwo from '@/components/_App/NavbarStyleTwo'
 import PageBannerStyle1 from '@/components/Common/PageBannerStyle1'
 import FooterStyleOne from '@/components/_App/FooterStyleOne'
 import {
@@ -31,41 +30,30 @@ const FAQ = () => {
                                 <AccordionItemHeading>
                                     <AccordionItemButton>
                                         <span>
-                                           FAQ
-                                        </span>
-                                    </AccordionItemButton>
-                                </AccordionItemHeading>
-
-                                {/* <AccordionItemPanel>
-                                    <p><strong>Texap</strong> is always looking for talented <a href="/faq">information</a> security and IT risk management professionals who are dedicated, hard working and looking for a challenge. If you are interested in employment with <strong>Texap</strong>, a company who values you and your family, visit our careers page.</p>
-                                    <ul>
-                                        <li>a console</li>
-                                        <li>Two Joy-Con controllers that are detachable</li>
-                                        <li>A grip that enables you to combine them into a single gamepad for play on the TV</li>
-                                        <li>Two straps for turning the Joy-Cons into individual controllers</li>
-                                        <li>A dock which you can use to connect your console to the television for traditional gameplay</li>
-                                    </ul>
-                                </AccordionItemPanel>
-                            </AccordionItem> */}
-
-                            <AccordionItem uuid="b">
-                                <AccordionItemHeading>
-                                    <AccordionItemButton>
-                                        <span>
-                                            What is the purpose of a consultant?
+                                        Minsa, when will we get our LoA and Invoice? Where was it sent?
                                         </span>
                                     </AccordionItemButton>
                                 </AccordionItemHeading>
 
                                 <AccordionItemPanel>
-                                    <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo.</p>
-                                    <ul>
-                                        <li>a console</li>
-                                        <li>Two Joy-Con controllers that are <a href="/faq">detachable</a></li>
-                                        <li>A grip that enables you to combine them into a single gamepad for play on the TV</li>
-                                        <li>Two straps for turning the Joy-Cons into <strong>individual</strong> controllers</li>
-                                        <li>A dock which you can use to connect your console to the television for traditional gameplay</li>
-                                    </ul>
+                                    <p>The committee will send LoA and Invoice no later than 7 working days after you register.
+                                    We will send the LoA and Invoice to the team leader's email, so make sure you include a valid and correct email address!</p>
+                                    
+                                </AccordionItemPanel>
+                            </AccordionItem>
+
+                            <AccordionItem uuid="b">
+                                <AccordionItemHeading>
+                                    <AccordionItemButton>
+                                        <span>
+                                        Minsa, I registered a week ago but how come I haven't received the LoA and Invoice yet?
+                                        </span>
+                                    </AccordionItemButton>
+                                </AccordionItemHeading>
+
+                                <AccordionItemPanel>
+                                    <p>Try checking the spam folder in your team leader's email, maybe the email from the IYSA team will go to the spam folder</p>
+                                    
                                 </AccordionItemPanel>
                             </AccordionItem>
 
@@ -73,14 +61,14 @@ const FAQ = () => {
                                 <AccordionItemHeading>
                                     <AccordionItemButton>
                                         <span>
-                                            What attracts you to the role of a consultant?
+                                        Where to upload the competition file, Minsa?
                                         </span>
                                     </AccordionItemButton>
                                 </AccordionItemHeading>
 
                                 <AccordionItemPanel>
-                                    <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor.</p>
-                                    <p>Tunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo.</p>
+                                    <p>Upload the files requested by the committee to the Google Drive link that we have included in your LoA, make sure you read the LoA carefully, OK!</p>
+                                    
                                 </AccordionItemPanel>
                             </AccordionItem>
 
@@ -88,19 +76,13 @@ const FAQ = () => {
                                 <AccordionItemHeading>
                                     <AccordionItemButton>
                                         <span>
-                                            What are the advantages of being a consultant?
+                                        I want to pay the registration fee but how come I can't find the invoice? and where is the payment transferred?
                                         </span>
                                     </AccordionItemButton>
                                 </AccordionItemHeading>
 
                                 <AccordionItemPanel>
-                                    <ul>
-                                        <li>a console</li>
-                                        <li>Two Joy-Con controllers that are detachable</li>
-                                        <li>A grip that enables you to combine them into a single gamepad for play on the TV</li>
-                                        <li>Two straps for turning the Joy-Cons into individual controllers</li>
-                                        <li>A dock which you can use to connect your console to the television for traditional gameplay</li>
-                                    </ul>
+                                    <p>Come on, have you read LoA to the end yet? Because we will send the invoice with your team's LoA, make sure you have read the email from us to the end! For payment, you can transfer to the account listed on the invoice. Make sure you pay according to the nominal stated on the invoice, okay!</p>
                                 </AccordionItemPanel>
                             </AccordionItem>
 
@@ -108,13 +90,13 @@ const FAQ = () => {
                                 <AccordionItemHeading>
                                     <AccordionItemButton>
                                         <span>
-                                            Is consulting a good career?
+                                        Minsa, I've already paid the registration fee. Where can I upload the proof of payment?
                                         </span>
                                     </AccordionItemButton>
                                 </AccordionItemHeading>
 
                                 <AccordionItemPanel>
-                                    <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo.</p>
+                                    <p>If you have made a payment, you can upload the proof to the link listed on the invoice, and make sure you fill in and upload the proof of transfer correctly so that the committee can record it correctly too.</p>
                                 </AccordionItemPanel>
                             </AccordionItem>
 
@@ -122,20 +104,28 @@ const FAQ = () => {
                                 <AccordionItemHeading>
                                     <AccordionItemButton>
                                         <span>
-                                            How is working in consulting?
+                                        When will the receipt be sent to us?
                                         </span>
                                     </AccordionItemButton>
                                 </AccordionItemHeading>
 
                                 <AccordionItemPanel>
-                                    <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo.</p>
-                                    <ul>
-                                        <li>a console</li>
-                                        <li>Two Joy-Con controllers that are <a href="/faq">detachable</a></li>
-                                        <li>A grip that enables you to combine them into a single gamepad for play on the TV</li>
-                                        <li>Two straps for turning the Joy-Cons into <strong>individual</strong> controllers</li>
-                                        <li>A dock which you can use to connect your console to the television for traditional gameplay</li>
-                                    </ul>
+                                    <p>We will send a receipt for payment in a maximum of 7 working days to the team leader's email, after you have uploaded proof of payment!</p>
+                                    
+                                </AccordionItemPanel>
+                            </AccordionItem>
+
+                            <AccordionItem uuid="g">
+                                <AccordionItemHeading>
+                                    <AccordionItemButton>
+                                        <span>
+                                        Minsa, one more week for judging, how come I haven't received my team's presentation schedule yet?
+                                        </span>
+                                    </AccordionItemButton>
+                                </AccordionItemHeading>
+
+                                <AccordionItemPanel>
+                                    <p>We will send the online participant presentation schedule no later than 2 days before the judging takes place to the whatsapp group and also the team leader's email. Make sure you diligently check the information we provide on the Whatsapp group and also email!</p>
                                 </AccordionItemPanel>
                             </AccordionItem>
                         </Accordion>
